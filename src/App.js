941f7f8be8ded23599596bf136856f0357096fba
Abhijeet Singh Thakur <abhiyayo@gmail.com>
2023-12-19 11:19:25 +0530

    Update App.js
    JSX use attributes with the HTML elements are same as the regular HTML.
    
    the naming convention in JSX is in camel case for attributes instead of standard naming convention of HTML becomes className in JSX.
    
    this is because the class is reserved keyword in JavaScript.
    
    import React , { Component } from 'react';
    class App extends Component {
    	render() {
    	   return(
    	      <div>
    	      <h1>Hello Students!!</h1>
    	      <h2>We are learning React.js Module</h2>
    	      <p data-customAttr = "demo">
    	      Use the link www.jetking.co/learn to access our MBD Course.
    	      </p>
    	      </div>
    	    );
           	}
    }
    export default App;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-class App extend Component {
+# JSX use attributes with the HTML elements are same as the regular HTML.
+
+# the naming convention in JSX is in camel case for attributes instead of standard naming convention of HTML becomes className in JSX.
+
+# this is because the class is reserved keyword in JavaScript.
+
+import React , { Component } from 'react';
+class App extends Component {
 	render() {
-		<div>
-		<h1>Hello Students!!</h1>
-		<h2>We are learning React.js Module</h2>
-		<p>Use the link www.jetking.co/learn to access our MBD Course.</p>
-		</div>
-	);
-	}
+	   return(
+	      <div>
+	      <h1>Hello Students!!</h1>
+	      <h2>We are learning React.js Module</h2>
+	      <p data-customAttr = "demo">
+	      Use the link www.jetking.co/learn to access our MBD Course.
+	      </p>
+	      </div>
+	    );
+       	}
 }
 export default App;
